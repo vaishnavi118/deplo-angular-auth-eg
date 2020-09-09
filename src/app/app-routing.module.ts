dcbc5e703538b055f995aace5f00e6d7006bc841
unknown <vaishnavi>
2020-09-09 12:20:02 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { AuthGuardService } from './auth-guard.service';
+
+import { HomeComponent } from './home/home.component';
+//import { GameComponent } from './game/game.component';
+
+const routes: Routes = [
+  {
+            path: '',
+            component: HomeComponent
+          },
+          {
+            path: 'login',
+            component: LoginComponent
+          },
+  //        {
+  //          path: 'game',
+  //          component: GameComponent,
+  //          canActivate: [ AuthGuardService ]
+  //        },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-auth-eg';
+  isAuthenticated: boolean;
+  constructor(public authService: AuthService) {
+    this.authService.isAuthenticated.subscribe(
+      (isAuthenticated: boolean)  => this.isAuthenticated = isAuthenticated
+    );
+  }
+  async ngOnInit() {
+    this.isAuthenticated = await this.authService.checkAuthenticated();
+  }
+  logout() {
+    this.authService.logout('/');
+  }
 }
